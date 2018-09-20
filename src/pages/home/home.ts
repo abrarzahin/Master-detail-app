2b06a430d0c8553dbbf3ef5efcf0e6682ea89979
abrar zahin <abrarzahin@yahoo.com>
2018-09-19 20:13:13 -0400

    4th

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { DetailPage } from '../detail/detail';
 import { PeopleData } from '../../providers/people-data';
-
+import{LoadingController} from 'ionic-angular';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
@@ -14,12 +14,17 @@ export class HomePage {
   public people = new Array();
   private detailPage;
 
-  constructor(public navCtrl: NavController, public peopleData:PeopleData) {
+  constructor(public navCtrl: NavController, public peopleData:PeopleData,public loadingCtrl:LoadingController) {
+   
+    let loader=this.loadingCtrl.create({content:'Loading People'});
+    loader.present();
+    this.detailPage = DetailPage;
     peopleData.getPeople().subscribe(people => {
       console.log('people',people);
+      loader.dismiss()
       this.people = people;
     });
-    this.detailPage = DetailPage;
+    
   }
 
   loadDetail(person) {
